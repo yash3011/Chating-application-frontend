0be1ff3843d238a917e92ac5ef2dce47b1f5792b
yash more <yash@gmail.com>
2026-09-08 14:08:07 +0530

    Use Turtle-Dove backend URL

--- public/api-config.js
+++ public/api-config.js
@@ -1,6 +1,6 @@
 const BACKEND_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000"
-    : "https://chating-application-backend-g2y6.onrender.com";
+    : "https://turtle-dove-backend.onrender.com";
 
 async function readResponse(response){
     const contentType = response.headers.get("content-type") || "";
